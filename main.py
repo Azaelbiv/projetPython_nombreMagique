6b9c0a64877038c97d0b19e12d53592b05aef6dc
Azael Bivihou <azaelbivihou1@gmail.com>
2023-07-11 11:42:50 +0100

    projet python: nombre magique

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-import turtle
+def demander_nombre(nb_min, nb_max):
+    nombre_int = 0
+    while nombre_int == 0:
+        nombre_str = input(f"Quel est le nombre magisue (entre {nb_min} et {nb_max}) ? ")
+        try:
+            nombre_int = int(nombre_str)
 
-t = turtle.Turtle()
-for i in range(0, 5):
-    t.forward(30)
-    t.left(90)
-    t.forward(30)
-    t.right(90)
-t.forward(30)
+        except ValueError:
+            print("Erreur: Vous devez rentrer un nombre: ressayez!")
 
-turtle.done()
+    return nombre_int
 
+
+NOMBRE_MIN = 1
+NOMBRE_MAX = 10
+NOMBRE8MAGIQUE = 5
+
+nombre = 0
+
+while not nombre == NOMBRE8MAGIQUE:
+    nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
+    if nombre == NOMBRE8MAGIQUE:
+        print("felicitations, vous avez gagné! \nLe nombre magique est bel et bien égal à " + str(nombre))
+
+    elif nombre > NOMBRE8MAGIQUE:
+        print("le nombre magique est plus petit que " + str(nombre))
+
+    elif nombre < NOMBRE8MAGIQUE:
+        print("le nombre magique est plus grand que " + str(nombre))
+    else:
+        print("Veillez entrer un nombre!")
