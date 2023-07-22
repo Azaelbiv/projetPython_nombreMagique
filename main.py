e96bf2f2bff227dee61cb091a39b223a4c5b489a
Azael Bivihou <azaelbivihou1@gmail.com>
2023-07-22 02:42:13 +0100

    utilisation de la fonction recursive et de la fonction lambda

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
-import numpy as np
+def myfunction(food):
+    for x in food:
+        print(x)
+
+
+fruits = ["mangues", "fraises", "bananes", "oranges", "ananas"]
+myfunction(fruits)
+
+print(type(fruits))
+
+
+# la fonction recursive
+def tri_recusive(k):
+    if k > 0:
+        resultat = k + tri_recusive(k - 1)
+        print(resultat)
+    else:
+        resultat = 0
+        return resultat
+
+
+#tri_recusive(4)
+
+# la fonction lambda(c'est une petite fonction anonime de la forme: lambda argument : expression)
+
+x = lambda nbre: nbre + 4
+reponse = x(7)
+print(f"la reponse de la valeur de lambda est : {reponse} ")
+
+
+def lambdafunct(n):
+    print()
+    return lambda x: x * n
+
+
+ledouble = lambdafunct(2)
+letriple = lambdafunct(3)
+print("le double de " + str(reponse) + " est " + str(ledouble(reponse)))
+print("le triple de " + str(reponse) + " est " + str(letriple(reponse)))
+
+
+
+"""import numpy as np
 import matplotlib.pyplot as plt
 
-circle = np.linspace(0, 2*np.pi, 40)
-x = np.cos(circle)
-y = np.sin(circle)
+tab1 = np.array([[1, 2, 3, 4], [2, 5, 3, 7]])
+tab2 = np.array([[4, 1, 2, 5], [8, 3, 6, 1]])
+a = tab2.T
+print(a)
+print(tab1, "\n", tab2)
+print(np.dot(tab1, a))
+print(tab1@a)
 
 
-fig, ax = plt.subplots()
-ax.plot(x, y)
-ax.axis("equal")
-ax.axis([-2, 2, -2, 2])
-plt.show()
-
-"""def demander_nombre(nb_min, nb_max):
+def demander_nombre(nb_min, nb_max):
     nombre_int = 0
     while nombre_int == 0:
         nombre_str = input(f"Quel est le nombre magisue (entre {nb_min} et {nb_max}) ? ")
